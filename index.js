5bd47e1822e0cadef448bdef1cf08dc20b689ec1
Lyra Freund <lyrafreund@gmail.com>
2023-09-12 16:18:52 -0600

    complete lab with notes

--- index.js
+++ index.js
@@ -4,3 +4,38 @@ const newHeader = document.createElement("h1")
 newHeader.setAttribute("id","victory")
 newHeader.textContent = "Lyra is the champion"
 document.body.append(newHeader)
+
+
+// const element = document.createElement("div")
+// document.body.append(element)
+
+// const ul = document.createElement("ul")
+
+// for (let i =0; i < 3; i++){
+//     const li = document.createElement("li")
+//     li.textContent = (i + 1).toString()
+//     li.textContent = "Hi there!"
+//     console.log(li.textContent)
+//     ul.append(li)
+// }
+
+
+// element.append(ul)
+
+// const main = document.getElementById("main")
+// main.innerHTML = "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>"
+ 
+// const element = document.getElementById("main")
+// element.style.height = "300px"
+// element.style.backgroundColor = "#27647B"
+// element.textContent = "you've changed the screen"
+// element.style.fontSize = "24px"
+// element.style.marginLeft = "30px"
+// element.style.lineHeight = 2
+// element.className = "pet-listing dog"
+// element.classList.remove("dog")
+// element.classList.add("cat", "sale")
+
+// const ul = document.getElementsByName("ul")[0]
+// const secondChild = ul.querySelector("li:nth-child(2)")
+// ul.removeChild(secondChild)
